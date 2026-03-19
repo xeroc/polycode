77bfe19123907e5d2262cfc0198602c8050176b3
Fabian Schuh <mail@xeroc.org>
2026-03-19 21:29:37 +0100

    ✨ feat: load persistence centrally

---
+++ apps/polycode/src/persistence/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class PersistenceSettings(BaseSettings):
+
+    # Database Configuration
+    DATABASE_URL: str = "sqlite:///polycode.db"
+
+    model_config = SettingsConfigDict(extra="ignore", env_file=".env", case_sensitive=True)
+
+
+settings = PersistenceSettings()  # pyright:ignore
